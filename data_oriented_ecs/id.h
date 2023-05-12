84c5be9fd6c5fa3c59a6d1872ccba825d831ee6c
Brandon Werner <brandonclayw@gmail.com>
2023-05-12 12:21:26 -0400

    Feat: added id functions

--- data_oriented_ecs/id.h
+++ data_oriented_ecs/id.h
@@ -19,4 +19,27 @@ namespace ecs::id {
 	using generation_type = std::conditional< (generation_bits > 16), u32, std::conditional< (generation_bits > 8), u16, u8 >::type >::type;
 	static_assert(sizeof(generation_type) * 8 >= generation_bits, "Generation bits must fit within a maximum of 32 bits");
 	static_assert((sizeof(id_type) - sizeof(generation_type)) > 0, "Generation bits must be less than total id bits");
+
+	inline bool is_valid(id_type id)
+	{
+		return id != id_mask;
+	}
+
+	inline id_type index(id_type id)
+	{
+		return id & index_mask;
+	}
+	
+	inline id_type generation(id_type id)
+	{
+		return (id & generation_mask) >> index_bits;
+	}
+
+	inline id_type new_generation(id_type id)
+	{
+		const id_type generation{ id::generation(id) + 1};
+		assert(generation < max_value_generation);
+		return id::index(id) | (generation << index_bits);
+	}
+
 }
--- data_oriented_ecs/main.cpp
+++ data_oriented_ecs/main.cpp
@@ -6,15 +6,24 @@
 
 int main()
 {
-	std::cout << "id_type:               " << sizeof(ecs::id::id_type) * 8 << " bits" << std::endl;
-	std::cout << "generation:            " << ecs::id::generation_bits << " bits" << std::endl;
-	std::cout << "index:                 " << ecs::id::index_bits << " bits" << std::endl;
-	std::cout << "index mask bits:       " << std::bitset<sizeof(ecs::id::id_type) * 8>(ecs::id::index_mask) << std::endl;
-	std::cout << "generation mask:       " << std::bitset<sizeof(ecs::id::id_type) * 8>(ecs::id::generation_mask) << std::endl;
-	std::cout << "id mask:               " << std::bitset<sizeof(ecs::id::id_type) * 8>(ecs::id::id_mask) << std::endl;
-	std::cout << "max generation value:  " << ecs::id::max_value_generation << std::endl;
-	std::cout << "max index value:       " << ecs::id::max_value_index << std::endl;
-	std::cout << "generation type:       u" << sizeof(ecs::id::generation_type) * 8 << std::endl;
+	std::cout << "id_type:                              " << sizeof(ecs::id::id_type) * 8 << " bits" << std::endl;
+	std::cout << "generation:                           " << ecs::id::generation_bits << " bits" << std::endl;
+	std::cout << "index:                                " << ecs::id::index_bits << " bits" << std::endl;
+	std::cout << "index mask bits:                      " << std::bitset<sizeof(ecs::id::id_type) * 8>(ecs::id::index_mask) << std::endl;
+	std::cout << "generation mask:                      " << std::bitset<sizeof(ecs::id::id_type) * 8>(ecs::id::generation_mask) << std::endl;
+	std::cout << "id mask:                              " << std::bitset<sizeof(ecs::id::id_type) * 8>(ecs::id::id_mask) << std::endl;
+	std::cout << "max generation value:                 " << ecs::id::max_value_generation << std::endl;
+	std::cout << "max index value:                      " << ecs::id::max_value_index << std::endl;
+	std::cout << "generation type:                      u" << sizeof(ecs::id::generation_type) * 8 << std::endl;
+
+	ecs::id::id_type entity{ 0 };
+	std::cout << "entity id bits:                       " << std::bitset<sizeof(ecs::id::id_type) * 8>(entity) << std::endl;
+	entity = ecs::id::new_generation(entity);
+	entity = ecs::id::new_generation(entity);
+	entity = ecs::id::new_generation(entity);
+	std::cout << "new generation entity id bits:        " << std::bitset<sizeof(ecs::id::id_type) * 8>(entity) << std::endl;
+	std::cout << "entity index:                         " << ecs::id::index(entity) << std::endl;
+	std::cout << "entity generation:                    " << ecs::id::generation(entity) << std::endl;
 
 	return EXIT_SUCCESS;
 }
